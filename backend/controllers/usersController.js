4a0b99a182b3f212f4cbe2a7a57d50d9d0073752
Yidan Cong <102019043+CongYidan@users.noreply.github.com>
2023-05-17 16:22:01 -0700

    async error handling

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,27 +1,26 @@
 const User = require('../models/User')
 const Note = require('../models/Note')
-const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcrypt')
 
 // @desc Get all users
 // @route GET /users
 // @access Private
-const getAllUsers = asyncHandler(async (req, res) => {
+const getAllUsers = async (req, res) => {
     const users = await User.find().select('-password').lean()
     if (!users.length) {
         return res.status(400).json({ message: 'No users found'})
     }
     res.json(users)
-})
+}
 
 // @desc Create new user
 // @route POST /users
 // @access Private
-const createNewUser = asyncHandler(async (req, res) => {
+const createNewUser = async (req, res) => {
     const { username, password, roles } = req.body
     
     //confirm data
-    if (!username || !password || !Array.isArray(roles) || !roles.length) {
+    if (!username || !password) {
         return res.status(400).json({ message: 'All fields are required'})
     }
 
@@ -35,7 +34,9 @@ const createNewUser = asyncHandler(async (req, res) => {
     //hash password
     const hashedPwd = await bcrypt.hash(password, 10) //salt rounds
     
-    const userObject = { username, "password": hashedPwd, roles }
+    const userObject = (!Array.isArray(roles) || !roles.length)
+        ? { username, "password": hashedPwd }
+        : { username, "password": hashedPwd, roles }
 
     //create and store new user
     const user = await User.create(userObject)
@@ -45,12 +46,12 @@ const createNewUser = asyncHandler(async (req, res) => {
     } else {
         res.status(400).json({ message: 'Invalid user data received'})
     }
-})
+}
 
 // @desc Update a user
 // @route PATCH /users
 // @access Private
-const updateUser = asyncHandler(async (req, res) => {
+const updateUser = async (req, res) => {
     const { id, username, roles, active, password } = req.body
 
     // confirm data
@@ -83,12 +84,12 @@ const updateUser = asyncHandler(async (req, res) => {
     const updatedUser = await user.save()
 
     res.json({ message: `${updatedUser.username} updated`})
-})
+}
 
 // @desc Delete a user
 // @route DELETE /users
 // @access Private
-const deleteUser = asyncHandler(async (req, res) => {
+const deleteUser = async (req, res) => {
     const { id } = req.body
     
     if (!id) {
@@ -111,7 +112,7 @@ const deleteUser = asyncHandler(async (req, res) => {
     const reply = `Username ${result.username} with ID ${result._id} deleted`
 
     res.json(reply)
-})
+}
 
 module.exports = {
     getAllUsers,
