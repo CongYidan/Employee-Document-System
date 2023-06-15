3ed9873c793b764ed86dcdc8384d820703fb3d3e
Yidan Cong <102019043+CongYidan@users.noreply.github.com>
2023-06-14 20:12:16 -0700

    css improvement

--- frontend/src/components/Public.js
+++ frontend/src/components/Public.js
@@ -4,21 +4,21 @@ const Public = () => {
     const content = (
         <section className="public">
             <header>
-                <h1>Welcome to <span className="nowrap">Dan D. Repairs!</span></h1>
+                <h1>Welcome to <span className="nowrap">Personal Journal Hive!</span></h1>
             </header>
             <main className="public__main">
-                <p>Located in Beautiful Downtown Foo City, Dan D. Repairs  provides a trained staff ready to meet your tech repair needs.</p>
+                <p>A secret hive developed by Danielle to store your personal journals!</p>
                 <address className="public__addr">
-                    Dan D. Repairs<br />
-                    555 Foo Drive<br />
-                    Foo City, CA 12345<br />
-                    <a href="tel:+15555555555">(555) 555-5555</a>
+                    Danielle Cong<br />
+                    San Jose, CA<br />
+                    MSCS @Northeastern University<br />
+                    <a href="mailto: congyd126@gmail.com">contact us</a>
                 </address>
                 <br />
-                <p>Owner: Dan Davidson</p>
+                <p>Owner: Danielle Cong</p>
             </main>
             <footer>
-                <Link to="/login">Employee Login</Link>
+                <Link to="/login">User Login</Link>
             </footer>
         </section>
 
